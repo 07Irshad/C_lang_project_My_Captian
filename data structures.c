83e5915d5961ec08ffbaa648e0fe970b33ca0aca
07Irshad <80592277+07Irshad@users.noreply.github.com>
2021-03-27 03:07:19 +0530

    Update data structures.c

--- data structures.c
+++ data structures.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 typedef struct
 {
-	char name[30];
+    char name[30];
     int id;
     int salary; 
 } Employee;
@@ -12,8 +12,8 @@ int main()
     Employee emp[n];
     printf("Enter %d Employee(s) Details \n",n);
     for(i=0; i<n; i++)
-	{
-		printf("\nEmployee %d\n",i+1);
+    {
+	printf("\nEmployee %d\n",i+1);
         printf("Name:   ");
         scanf("%s",emp[i].name);
         printf("Id:   ");
@@ -22,8 +22,8 @@ int main()
         scanf("%d",&emp[i].salary);
     }
     for(i=0; i<n; i++)
-	{
-		printf("\nName  : ");
+    {
+	printf("\nName  : ");
         printf("%s \n",emp[i].name);
         printf("Id    : ");
         printf("%d \n",emp[i].id);
